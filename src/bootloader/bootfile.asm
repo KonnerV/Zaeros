ee895f23db1211412b483f8c77130066ffd0dbaf
Konner V <94082086+KonnerV@users.noreply.github.com>
2023-09-23 03:16:22 +0100

    Rewrote the bootloader to load the kernel at an earlier stage.
    Rewrote the bootloader to load the kernel at an earlier stage.

--- src/bootloader/bootfile.asm
+++ src/bootloader/bootfile.asm
@@ -1,34 +1,20 @@
 [bits 16]
 [org 0x7c00]
 boot:
-    mov si, string
+    mov si, bl_ok
     call printstr
+    
+    jmp load_kernel
 
-    call init_unreal
-unreal:
-    pop ds ; restoring the old data-segment
-    sti ; re-enabling BIOS interrupts
-
-    mov eax, 0x0b8000 ; VGA-Buffer address
-
-    ; Prints "UR" to the VGA to show that it has worked.
-    mov word [ds:eax], 0x0155
-    mov word [ds:eax+0x02], 0x0152
-
-    call cmd_init
-    jmp $    
+    jmp $
 
 .hlt:
     cli ; clear interrupts
     hlt ; stop the CPU
 
-%include "unrealmode_entry.asm"
 %include "boot_print.asm"
-%include "bios_keyboard.asm"
-%include "bootloader_cmd.asm"
-string: db "Hello, KV-Bootloader!", 0xa, 0xd, 0
-
-
+%include "read_kernel.asm"
+bl_ok: db "KV-Boot [Works]", 0xa, 0xd, 0
 times 510-($-$$) db 0
 ; Boot signature
 dw 0xaa55
---
+++ src/bootloader/read_kernel.asm
@@ -0,0 +1,37 @@
+load_kernel:
+    xor ax, ax
+    mov ds, ax
+
+    cli
+    mov ss, ax
+    mov sp, 0x7c00
+    sti
+
+    xor ax, ax
+    int 0x13
+    jc .dsk_err
+
+    mov ax, 0x1000
+    xor bx, bx
+    mov es, bx
+    mov bx, 0x1000
+
+    mov ah, 0x02 ;2
+    mov al, 0x01
+    mov cl, 0x01
+    mov ch, 0
+    mov dh, 0
+    int 0x13
+    jc .dsk_err
+    cmp al, 1
+    jne .dsk_err
+
+    mov si, dsk_ok
+    call printstr
+    jmp $
+
+.dsk_err:
+    mov si, err_msg
+    call printstr
+dsk_ok: db "Disk [Works]", 0xa, 0xd, 0
+err_msg: db "Error reading 0x1000 from disk", 0xa, 0xd, 0
