68fc8597da4ba04cfd0d77b87fbd8cb1405327d9
Konner V <94082086+KonnerV@users.noreply.github.com>
2023-09-30 20:28:58 +0100

    Updated build.sh
    Updated build.sh to reflect the new changes.

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ nasm bootfile.asm -f bin -o bootloader.bin
 nasm kernel_entry.asm -f elf -o kernel_entry.o
 gcc -g -m32 -fno-pie -ffreestanding -c kernel.c -o kernel.o
 gcc -g -m32 -fno-pie -ffreestanding -c vga_write.c -o vga_write.o
+gcc -g -m32 -fno-pie -ffreestanding -c io.c -o io.o
+gcc -g -m32 -fno-pie -ffreestanding -c pc_spkr.c -o pc_spkr.o
 # Linking the Kernel
-ld -m elf_i386 -o krnl.bin -Ttext 0x1000 kernel_entry.o kernel.o vga_write.o --oformat binary
+ld -m elf_i386 -o krnl.bin -Ttext 0x1000 kernel_entry.o kernel.o io.o pc_spkr.o vga_write.o --oformat binary
 
 # Merging the Kernel and Bootloader
 cat bootloader.bin krnl.bin > os.bin
 
 # Emulating it
-qemu-system-i386 -fda os.bin
+qemu-system-x86_64 -hda os.bin
