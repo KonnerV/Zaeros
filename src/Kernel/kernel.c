9d523800e292366840faf336309d5e8c1a5c2aa4
Konner V <94082086+KonnerV@users.noreply.github.com>
2023-10-18 23:25:41 +0100

    Changing the OS name.

--- src/Kernel/kernel.c
+++ src/Kernel/kernel.c
@@ -6,9 +6,13 @@
 void panic() {
     char panic_msg[] = "Error! Panic resulted in halt.\0";
     vga_writestr(panic_msg, get_colour_code(Black, Red), strlen(panic_msg));
-    for (;;) {
-        beep_at_freq(500);
-    }
+    for (;;) {beep_at_freq(500);}
+}
+
+__attribute__((noreturn))
+void exception_handler(void);
+void exception_handler() {
+    __asm__ volatile("cli;hlt");
 }
 
 typedef struct {
@@ -29,9 +33,14 @@ void write_ent(uint16_t num, uint32_t handle) {
     interrupt_descriptor_table[num].offtwo = (uint16_t)(((handle)>>16)&0xffff);
 }
 
+void wmemloc(uint8_t* loc, char data[]) {
+    for(int i=0;i<strlen(data);i++)
+        *loc++ = data[i]; 
+}
+
 void kernel_main(void) {
     clr_scrn();
-    char wlcm_msg[] = "Welcome to KV-OS x86\0";
+    char wlcm_msg[] = "Welcome to Zaeros x86\0";
 
     beep_at_freq(1000);
     vga_writestr(wlcm_msg, get_colour_code(Black, White), strlen(wlcm_msg));
